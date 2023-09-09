e70f1c4c0eca38da923b1c164d8bfd962afd4b44
Your Name <you@example.com>
2023-09-10 01:01:52 +0530

    push

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '78c0fbb9ddmsh3e74fe06b513817p14d65ajsn43e6b41eca15',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
